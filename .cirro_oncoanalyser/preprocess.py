fa0924a5d6f00cde4801117485ea086aa1c784d9
Peter Zheng <youyunzheng@hms.harvard.edu>
2025-09-10 14:37:39 -0400

    get dna/rnaseq and fastq1/2 from ds.files. Now it should be scalable across many runs

--- .cirro_oncoanalyser/preprocess.py
+++ .cirro_oncoanalyser/preprocess.py
@@ -4,6 +4,7 @@ from cirro.helpers.preprocess_dataset import PreprocessDataset
 import pandas as pd
 import numpy as np
 import re
+pd.set_option('display.max_columns', None)
 
 # 1. Get parameters from cirro pipeline call
 ds = PreprocessDataset.from_running()
@@ -34,24 +35,26 @@ ds.logger.info(ds.samplesheet)
 # filepath is the concatenation of fastq_1 and fastq_2 with a ; in between
 # there is not library ID or lane information in ds.samplesheet so we will just use row index instead so every sample is unique
 
-pd.set_option('display.max_columns', None)
+# dcast ds.files based on sample column, and read column indicates fastq_1 and fastq_2, use file as the content column
+# this assumes that each sample has exactly one fastq_1 and one fastq_2
+pivoted_file_list = ds.files.pivot(index=['sample','process'], columns='read', values='file').reset_index()
+# rename column names to be more descriptive
+pivoted_file_list.columns = ['sample', 'process', 'fastq_1', 'fastq_2']
+pivoted_file_list['fastq_1_basename'] = pivoted_file_list['fastq_1'].apply(lambda x: x.split('/')[-1])
+pivoted_file_list['fastq_2_basename'] = pivoted_file_list['fastq_2'].apply(lambda x: x.split('/')[-1])
+print(pivoted_file_list)
+
 samplesheet = pd.DataFrame(columns=["group_id","subject_id","sample_id","sample_type","sequence_type","filetype","info","filepath"])
-samplesheet['filepath'] = ds.samplesheet['fastq_1'].astype(str) + ';' + ds.samplesheet['fastq_2'].astype(str)
-print(samplesheet)
-samplesheet['sample_id'] = [re.sub('_','.', sample_id) for sample_id in ds.samplesheet['sample'].astype(str)]
+samplesheet['filepath'] = pivoted_file_list['fastq_1'].astype(str) + ';' + pivoted_file_list['fastq_2'].astype(str)
+samplesheet['sample_id'] = [re.sub('_','.', sample_id) for sample_id in pivoted_file_list['sample'].astype(str)]
+samplesheet['sequence_type'] = ['dna' if 'dna' in process else 'rna' for process in pivoted_file_list['process']]
+
+
 samplesheet['group_id'] = [re.split(r'[._]', sample_id)[1] for sample_id in samplesheet['sample_id']]
 samplesheet['subject_id'] = samplesheet['group_id']
 samplesheet['info'] = ['library_id:S' + str(i+1) + ';lane:' + str(i+1) for i in range(len(samplesheet))]
-print(samplesheet)
 samplesheet['sample_type'] = ['normal' if 'PBMC' in sample_id else 'tumor' for sample_id in samplesheet['sample_id']]
 samplesheet['filetype'] = ['fastq' for _ in samplesheet['filepath']]
-print(samplesheet)
-
-# this run specifically -- NOT ALWAYS TRUE
-samplesheet['sequence_type'] = ['dna' if 'merged' in path else 'rna' for path in samplesheet['filepath']]
-print(samplesheet)
-
-# add -RNA to sample id for rows that are rna
 samplesheet.loc[samplesheet['sequence_type'] == 'rna', 'sample_id'] = samplesheet['sample_id'] + '-RNA'
 print(samplesheet)
 
